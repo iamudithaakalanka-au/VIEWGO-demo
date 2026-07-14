52eb1c4b853b3a32e1a6a1ed363288400447cbdd
Uditha Akalanka <udithaakalankaofficial@gmail.com>
2026-07-14 14:15:23 +1000

    Add files via upload

---
+++ destinations/blueMountains/blueMountains.js
@@ -0,0 +1,154 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+/* Show menu */
+if(navToggle){
+   navToggle.addEventListener('click', () =>{
+      navMenu.classList.add('show-menu')
+   })
+}
+
+/* Hide menu */
+if(navClose){
+   navClose.addEventListener('click', () =>{
+      navMenu.classList.remove('show-menu')
+   })
+}
+
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link')
+
+const linkAction = () =>{
+   const navMenu = document.getElementById('nav-menu')
+   // When we click on each nav__link, we remove the show-menu class
+   navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*=============== CHANGE HEADER STYLES ===============*/
+
+
+/*=============== SWIPER HOME ===============*/
+const swiperHome = new Swiper('.home__swiper', {
+  loop: true,
+  slidesPerView: 'auto',
+  grabCursor: true,
+
+  navigation: {
+    nextEl: '.home__button-next',
+    prevEl: '.home__button-prev',
+  },
+
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  }
+
+});
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+const bgHeader = () =>{
+   const header = document.getElementById('header')
+   this.scrollY >= 50 ? header.classList.add('bg-header') 
+                      : header.classList.remove('bg-header')
+}
+window.addEventListener('scroll', bgHeader)
+
+
+/*=============== SWIPER reviews ===============*/
+const swiperReviews = new Swiper('.reviews__swiper', {
+  loop: true,
+  slidesPerView: 'auto',
+  spaceBetween: 48,
+  grabCursor: true,
+
+  navigation: {
+    nextEl: '.reviews__button-next',
+    prevEl: '.reviews__button-prev',
+  },
+
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  }
+
+});
+
+/*=============== SHOW SCROLL UP ===============*/ 
+const scrollUp = () =>{
+	const scrollUp = document.getElementById('scroll-up')
+	this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+						     : scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+
+const scrollActive = () => {
+   const scrollDown = window.scrollY
+
+   sections.forEach(section => {
+      const sectionHeight = section.offsetHeight, 
+            sectionTop = section.offsetTop - 58, 
+            sectionId = section.getAttribute('id'), 
+            sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+      if(!sectionsClass) return
+
+      if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight)  {
+        sectionsClass.classList.add('active-link')
+      }else{
+        sectionsClass.classList.remove('active-link')
+      }
+   })
+}
+window.addEventListener('scroll', scrollActive)
+
+/*=============== DARK LIGHT THEME ===============*/ 
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'ri-sun-fill'
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'ri-moon-fill' : 'ri-sun-fill'
+
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+  themeButton.classList[selectedIcon === 'ri-moon-fill' ? 'add' : 'remove'](iconTheme)
+}
+
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    // We save the theme and the current icon that the user chose
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal ({
+  origin: 'top',
+  distance: '60px',
+  duration: 2000,
+  delay: 300,
+  // reset: true, // Animation repeat
+})
+
+sr.reveal('.home__container, .reviews__container, .footer__container')
+sr.reveal('.home__title', {delay: 600})
+sr.reveal('.home__info', {delay: 800})
+sr.reveal('.home__actions', {delay: 1000})
+sr.reveal('.highlights__container')
+sr.reveal('.contact__data', {origin: 'left'})
+sr.reveal('.contact__img', {origin: 'right'})
